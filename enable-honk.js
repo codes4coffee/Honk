3033cbb2fcf77f706e55a57ee8d4bb2d7652db41
codes4coffee <jakelanc950@gmail.com>
2020-02-11 14:25:03 -0600

    Changed title and text below search bar

--- enable-honk.js
+++ enable-honk.js
@@ -1,1 +1,3 @@
 document.getElementById("logo_homepage_link").style.backgroundImage = 'url("https://firefox-honk-extension.s3.amazonaws.com/honk.png")';
+document.getElementsByClassName("tag-home__item")[0].innerHTML = 'Peace was never an option';
+document.title = 'DuckDuckGoose';
